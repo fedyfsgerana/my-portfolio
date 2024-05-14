c844b18abfeae72b61d21158389d79b898c31991
fedyfsgerana <eddiebrock035@gmail.com>
2024-05-14 11:22:26 +0700

    Edit content

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light site-navbar-target"
     id="ftco-navbar">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('profile') }}">Clyde<span>.</span></a>
+        <a class="navbar-brand" href="{{ url('profile') }}">Fedy<span>.</span></a>
         <button class="navbar-toggler js-fh5co-nav-toggle fh5co-nav-toggle" type="button" data-toggle="collapse"
             data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="oi oi-menu"></span> Menu
@@ -12,9 +12,7 @@
                 <li class="nav-item"><a href="{{ url('#home-section') }}" class="nav-link"><span>Home</span></a></li>
                 <li class="nav-item"><a href="{{ url('#about-section') }}" class="nav-link"><span>About</span></a></li>
                 <li class="nav-item"><a href="{{ url('#skills-section') }}" class="nav-link"><span>Skills</span></a></li>
-                <li class="nav-item"><a href="{{ url('#services-section') }}" class="nav-link"><span>Services</span></a></li>
                 <li class="nav-item"><a href="{{ url('#projects-section') }}" class="nav-link"><span>Projects</span></a></li>
-                <li class="nav-item"><a href="{{ url('#blog-section') }}" class="nav-link"><span>Blog</span></a></li>
                 <li class="nav-item"><a href="{{ url('#contact-section') }}" class="nav-link"><span>Contact</span></a></li>
             </ul>
         </div>
