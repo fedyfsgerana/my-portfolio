2d14333c60ae356ecf9c05d93ff180fcf8c6b39f
fedyfsgerana <eddiebrock035@gmail.com>
2024-05-14 16:24:57 +0700

    Edit content

--- resources/views/layouts/content.blade.php
+++ resources/views/layouts/content.blade.php
@@ -4,7 +4,7 @@
 <section id="home-section" class="hero">
     <div class="home-slider owl-carousel">
         <div class="slider-item">
-            <div class="overlay"></div>COMPLETE
+            <div class="overlay"></div>
             <div class="container-fluid px-md-0">
                 <div class="row d-md-flex no-gutters slider-text align-items-end justify-content-end"
                     data-scrollax-parent="true">
@@ -42,6 +42,57 @@
                             <p><a href="#" class="btn btn-primary">Hire me</a> <a href="#"
                                     class="btn btn-primary btn-outline-primary">Download CV</a></p>
                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ftco-counter img bg-light" id="section-counter">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 justify-content-center counter-wrap ftco-animate">
+                <div class="block-18 d-flex">
+                    <div class="icon d-flex justify-content-center align-items-center">
+                        <span class="flaticon-suitcase"></span>
+                    </div>
+                    <div class="text">
+                        <strong class="number" data-number="750">0</strong>
+                        <span>Project Complete</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 justify-content-center counter-wrap ftco-animate">
+                <div class="block-18 d-flex">
+                    <div class="icon d-flex justify-content-center align-items-center">
+                        <span class="flaticon-loyalty"></span>
+                    </div>
+                    <div class="text">
+                        <strong class="number" data-number="568">0</strong>
+                        <span>Happy Clients</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 justify-content-center counter-wrap ftco-animate">
+                <div class="block-18 d-flex">
+                    <div class="icon d-flex justify-content-center align-items-center">
+                        <span class="flaticon-coffee"></span>
+                    </div>
+                    <div class="text">
+                        <strong class="number" data-number="478">0</strong>
+                        <span>Cups of coffee</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 justify-content-center counter-wrap ftco-animate">
+                <div class="block-18 d-flex">
+                    <div class="icon d-flex justify-content-center align-items-center">
+                        <span class="flaticon-calendar"></span>
+                    </div>
+                    <div class="text">
+                        <strong class="number" data-number="780">0</strong>
+                        <span>Years experienced</span>
                     </div>
                 </div>
             </div>
