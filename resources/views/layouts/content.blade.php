8e745d15f6650a5aaa8d007443c59777d37a95ba
fedyfsgerana <eddiebrock035@gmail.com>
2024-05-16 15:35:13 +0700

    Rapikan kode

--- resources/views/layouts/content.blade.php
+++ resources/views/layouts/content.blade.php
@@ -22,8 +22,10 @@ Home
                         <div class="text">
                             <span class="subheading">Hello! My Name Is Fedy</span>
                             <h1 class="mb-4 mt-3" style="color: #000000;">Web Development &amp; Building Designer</h1>
-                            <p> <a href="#" class="btn btn-primary">Hire me</a>
-                                <a href="#" class="btn btn-primary btn-outline-primary">Download CV</a></p>
+                            <p>
+                                <a href="#" class="btn btn-primary">Hire me</a>
+                                <a href="#" class="btn btn-primary btn-outline-primary">Download CV</a>
+                            </p>
                         </div>
                     </div>
                 </div>
